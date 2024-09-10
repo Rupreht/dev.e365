cbb9b7f22c9001efd9ef23d0d58ffb5dafc389c8
Nikolay Ermolovich <git@rupreht.ru>
2024-09-11 02:25:52 +0500

    fix

--- e365/urls.py
+++ e365/urls.py
@@ -1,27 +1,47 @@
-"""e365 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
+import django
+from django.apps import apps
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.contrib.sitemaps import views
+from django.urls import include, path
+from oscar.views import handler403, handler404, handler500
 
-# from debug_toolbar.toolbar import debug_toolbar_urls
+from shop.sitemaps import base_sitemaps
+
+admin.autodiscover()
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # Include admin as convenience. It's unsupported and only included
+    # for developers.
+    path('admin/', admin.site.urls),
+
+    # i18n URLS need to live outside of i18n_patterns scope of Oscar
+    path('i18n/', include(django.conf.urls.i18n)),
+
+    # include a basic sitemap
+    path('sitemap.xml', views.index,
+         {'sitemaps': base_sitemaps}),
+    path('sitemap-<slug:section>.xml', views.sitemap,
+         {'sitemaps': base_sitemaps},
+         name='django.contrib.sitemaps.views.sitemap')
 ]
 
-# Debug
-# urlpatterns + debug_toolbar_urls()
+# Prefix Oscar URLs with language codes
+urlpatterns += i18n_patterns(
+    path('', include(apps.get_app_config('oscar').urls[0])),
+)
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    # Server statics and uploaded media
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Allow error pages to be tested
+    urlpatterns += [
+        path('403', handler403, {'exception': Exception()}),
+        path('404', handler404, {'exception': Exception()}),
+        path('500', handler500),
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
