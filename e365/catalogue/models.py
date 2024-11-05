4f065f5d81889be87844530d5ba8fea9f7bb616a
Nikolay Ermolovich <git@rupreht.ru>
2024-11-05 05:30:45 +0500

    added video_url to Product

---
+++ e365/catalogue/models.py
@@ -0,0 +1,11 @@
+# e365/catalogue/models.py
+
+from django.db import models
+from oscar.apps.catalogue.abstract_models import AbstractProduct
+
+
+class Product(AbstractProduct):
+    video_url = models.URLField(default='')
+
+
+from oscar.apps.catalogue.models import *  # pylint: disable=wildcard-import,unused-wildcard-import
